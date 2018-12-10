c3c51b2342b865a8c13e0d5ab711908e28e1082a
Lleyton <lleyton92@gmail.com>
2018-12-10 11:01:51 +1100

    added basic nuget packages and startup code

--- ChaosGame/Program.cs
+++ ChaosGame/Program.cs
@@ -3,19 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
 
-namespace ChaosGame
+namespace StarterKit
 {
-    class Program
+    class Game : GameWindow
     {
-        static void Main(string[] args)
+        public Game() : base(800, 600, GraphicsMode.Default, "OpenTK Quick Start Sample")
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+           VSync = VSyncMode.On;
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            GL.ClearColor(0.1f, 0.2f, 0.5f, 0.0f);
+            GL.Enable(EnableCap.DepthTest);
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1.0f, 64.0f);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
+        }
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (Keyboard.GetState()[Key.Escape])
+                Exit();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            SwapBuffers();
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            using (Game game = new Game())
+            {
+                game.Run(30.0);
+            }
         }
     }
 }
